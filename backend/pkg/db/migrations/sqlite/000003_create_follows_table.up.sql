e414ba279d4f4ae23c8c5658cbe202434963fcad
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-09-14 22:30:31 +0100

    add new data base tables

--- backend/pkg/db/migrations/sqlite/000003_create_follows_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000003_create_follows_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS follows (
     follower_id INTEGER NOT NULL,
     following_id INTEGER NOT NULL,
+    status TEXT NOT NULL DEFAULT 'accepted', -- pending | accepted
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (follower_id, following_id),
-    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
+    PRIMARY KEY(follower_id, following_id),
+    FOREIGN KEY(follower_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(following_id) REFERENCES users(id) ON DELETE CASCADE
 );
