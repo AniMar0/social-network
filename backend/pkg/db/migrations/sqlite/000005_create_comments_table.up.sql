6ab3e8d198261a6a140bb5dad378a1f8649a37fe
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-09-28 12:05:08 +0100

    refactor comments tabel to support nested comments

--- backend/pkg/db/migrations/sqlite/000005_create_comments_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000005_create_comments_table.up.sql
@@ -2,8 +2,10 @@ CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
+    parent_id INTEGER,
     content TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    FOREIGN KEY(parent_id) REFERENCES comments(id) ON DELETE CASCADE,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
