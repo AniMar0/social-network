d9f534579b1c0ca3d49852cf8dc44ea1e32c88f6
AniMar0 <mr.zakariakahlaoui@gmail.com>
2025-09-28 12:22:42 +0100

    comments handler and insert comment into DB

--- backend/pkg/api/Comments.go
+++ backend/pkg/api/Comments.go
@@ -19,7 +19,7 @@ func (S *Server) CreateCommentHandler(w http.ResponseWriter, r *http.Request) {
 
 	var body struct {
 		Content         string `json:"content"`
-		ParentCommentId int    `json:"parentCommentId,omitempty"`
+		ParentCommentId *int   `json:"parentCommentId,omitempty"`
 		PostID          int    `json:"postID"`
 	}
 
@@ -29,10 +29,22 @@ func (S *Server) CreateCommentHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//S.CreateComment(currentUserID, body.Content, body.PostID, body.ParentCommentId)
+	err = S.CreateComment(currentUserID, body.Content, body.PostID, body.ParentCommentId)
+	if err != nil {
+		http.Error(w, "Failed to create comment", http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "Comment created successfully",
 	})
+}
+
+func (S *Server) CreateComment(userID int, content string, postID int, parentCommentID *int) error {
+	_, err := S.db.Exec("INSERT INTO comments (user_id, content, post_id, parent_comment_id) VALUES (?, ?, ?, ?)", userID, content, postID, parentCommentID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- backend/pkg/db/migrations/sqlite/000005_create_comments_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000005_create_comments_table.up.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     post_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
-    parent_id INTEGER,
+    parent_comment_id INTEGER,
     content TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    FOREIGN KEY(parent_id) REFERENCES comments(id) ON DELETE CASCADE,
+    FOREIGN KEY(parent_comment_id) REFERENCES comments(id) ON DELETE CASCADE,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
